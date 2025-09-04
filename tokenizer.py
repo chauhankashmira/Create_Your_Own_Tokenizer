1b7d84ad5041db93c066241980b3f1d06ea101cd
Kash Chauhan <chauhankashmira@yahoo.co.uk>
2025-09-04 11:19:01 +0100

    tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['HF_TOKEN'] = "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"
+os.environ['HF_TOKEN'] = "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" //please do not use this token as it has been removed after testing and will not work for you
 
 from transformers import AutoTokenizer
 
